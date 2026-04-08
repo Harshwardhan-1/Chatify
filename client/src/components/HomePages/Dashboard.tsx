ab12b5df747294296db76867b868341b990005f7
harsh <harshwardhany87@gmail.com>
2026-04-08 22:04:15 +0530

    use useChatUseEffect that help for better code readability

--- client/src/components/HomePages/Dashboard.tsx
+++ client/src/components/HomePages/Dashboard.tsx
@@ -1,10 +1,11 @@
-  import {useState, useEffect } from "react";
-  import { env } from "../../configs/env.config";
-  import axios,{AxiosError} from "axios";
-  import '../../styles/Dashboard.css';
+import {UserUseEffect} from '../../hooks/usechatUseEffect';
+import '../../styles/Dashboard.css';
   import { useNavigate } from "react-router-dom";
   export function Dashboard(){
     const navigate=useNavigate();
+
+
+    const {data,convo,id}=UserUseEffect();
     interface userInfo{
       userId:string,
       name:string,
@@ -12,96 +13,6 @@
       email:string,
       profilePic:string,
     }
-  const [data,setData]=useState<userInfo[]>([]);
-    useEffect(()=>{
-      const fetch=async()=>{
-        try{
-          const response=await axios.get(`${env.backendurl}/api/v1/userInfo`,{withCredentials:true});
-          if(response.data.message=== 'successfull'){
-            setData(response.data.data);
-          }
-        }catch(err){
-          const error=err as AxiosError;
-          if(error.response && error.response.data){
-            const data=error.response.data as {error:string;message:string};
-            alert(data.error || data.message || 'somethig went wrong');
-          }else{
-            alert(error.message);
-          }
-        }
-      };
-      fetch();
-    })
-
-
-
-
-
-  //getted all users that user previously talk
-  interface Conversion{
-    participants:string[], //senderId,recieverId hongi esma user ki
-    lastMessage:string,
-    createdAt:Date,
-  }
-  const [convo,setConvo]=useState<Conversion[]>([]);
-    useEffect(()=>{
-      const fetch=async()=>{
-  try{
-  const response=await axios.get(`${env.backendurl}/api/v1/lastMessage`,{withCredentials:true});
-  if(response.data.message=== 'successfull'){
-    setConvo(response.data.data);
-  }
-  }catch(err){
-    const error=err as AxiosError;
-    if(error.response && error.response.data){
-      const data=error.response.data as {error:string;message:string};
-      alert(data.error || data.message || 'something went wrong');
-    }else{
-      alert(error.message);
-    }
-  }
-      };
-      fetch();
-    },[]);
-
-
-
-
-
-
-
-
-
-
-
-  //currentUserid
-  interface userId{
-    currentUserId:string,
-  }
-  const [id,setId]=useState<userId>();
-  useEffect(()=>{
-    const fetch=async()=>{
-      try{
-        const response=await axios.get(`${env.backendurl}/api/v1/getCurrentUserId`,{withCredentials:true});
-        if(response.data.message=== 'successfull'){
-          setId(response.data.data);
-        }
-      }catch(err){
-        const error=err as AxiosError;
-        if(error.response && error.response.data){
-          const data=error.response.data as {error:string;message:string};
-          alert(data.message || data.error || 'something went wrong');
-        }else{
-          alert(error.message);
-        }
-      }
-    };
-    fetch();
-  },[])
-
-
-
-
     const handleClick=async(all:userInfo)=>{
       navigate('/ChatPage',{state:{harsh:all}});
     }
---
+++ client/src/hooks/usechatUseEffect.tsx
@@ -0,0 +1,91 @@
+import {useState} from 'react';
+import { useEffect } from 'react';
+import { env } from '../configs/env.config';
+import axios,{AxiosError} from 'axios';
+
+
+interface userInfo{
+      userId:string,
+      name:string,
+      userName:string,
+      email:string,
+      profilePic:string,
+    }
+
+    
+  interface Conversion{
+    participants:string[], 
+    lastMessage:string,
+    createdAt:Date,
+  }
+
+  interface userId{
+    currentUserId:string,
+  }
+export const UserUseEffect=()=>{
+ const [data,setData]=useState<userInfo[]>([]);
+ const [convo,setConvo]=useState<Conversion[]>([]);
+   const [id,setId]=useState<userId>();
+
+    useEffect(()=>{
+      const fetch=async()=>{
+        try{
+          const response=await axios.get(`${env.backendurl}/api/v1/userInfo`,{withCredentials:true});
+          if(response.data.message=== 'successfull'){
+            setData(response.data.data);
+          }
+        }catch(err){
+          const error=err as AxiosError;
+          if(error.response && error.response.data){
+            const data=error.response.data as {error:string;message:string};
+            alert(data.error || data.message || 'somethig went wrong');
+          }else{
+            alert(error.message);
+          }
+        }
+      };
+      fetch();
+    },[]);
+
+
+    useEffect(()=>{
+      const fetch=async()=>{
+  try{
+  const response=await axios.get(`${env.backendurl}/api/v1/lastMessage`,{withCredentials:true});
+  if(response.data.message=== 'successfull'){
+    setConvo(response.data.data);
+  }
+  }catch(err){
+    const error=err as AxiosError;
+    if(error.response && error.response.data){
+      const data=error.response.data as {error:string;message:string};
+      alert(data.error || data.message || 'something went wrong');
+    }else{
+      alert(error.message);
+    }
+  }
+      };
+      fetch();
+    },[]);
+     useEffect(()=>{
+    const fetch=async()=>{
+      try{
+        const response=await axios.get(`${env.backendurl}/api/v1/getCurrentUserId`,{withCredentials:true});
+        if(response.data.message=== 'successfull'){
+          setId(response.data.data);
+        }
+      }catch(err){
+        const error=err as AxiosError;
+        if(error.response && error.response.data){
+          const data=error.response.data as {error:string;message:string};
+          alert(data.message || data.error || 'something went wrong');
+        }else{
+          alert(error.message);
+        }
+      }
+    };
+    fetch();
+  },[])
+
+  return {data,convo,id};
+}
